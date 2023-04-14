430af31846a3a5af174c4a193c556ee4bd9ba9fe
Goutamkurri <123128235+Goutamkurri@users.noreply.github.com>
2023-04-13 23:15:24 -0500

    updated racecar.js file

--- controllers/racecar.js
+++ controllers/racecar.js
@@ -15,9 +15,25 @@ exports.racecar_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: racecar detail: ' + req.params.id);
 };
 // Handle racecar create on POST.
-exports.racecar_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: racecar create POST');
-};
+exports.racecar_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new racecar();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"racecar_type":"goat", "cost":12, "size":"large"}
+    document.driver = req.body.driver;
+    // document.constructor = req.body.constructor;
+    document.ranking = req.body.ranking;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // Handle racecar delete form on DELETE.
 exports.racecar_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: racecar delete DELETE ' + req.params.id);

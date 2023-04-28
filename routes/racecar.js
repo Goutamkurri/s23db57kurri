39d782a25295d5e29ada0b8a63ba7106a0d2df1e
Goutamkurri <123128235+Goutamkurri@users.noreply.github.com>
2023-04-27 21:52:33 -0500

    updated racecar.js

--- routes/racecar.js
+++ routes/racecar.js
@@ -16,12 +16,12 @@ router.get('/', racecar_controlers.racecar_view_all_Page );
 module.exports = router;
 /* GET detail racecar page */
 router.get('/detail', racecar_controlers.racecar_view_one_Page);
+
 /* GET create racecar page */
-router.get('/create',secured, racecar_controlers.racecar_create_Page);
-/* GET create update page */
-// router.get('/update', racecar_controlers.racecar_update_Page);
+router.get('/create', secured, racecar_controlers.racecar_create_Page);
+
 /* GET delete racecar page */
-router.get('/delete',secured, racecar_controlers.racecar_delete_Page);
+router.get('/delete', secured, racecar_controlers.racecar_delete_Page);
 
 /* GET update racecar page */
 router.get('/update', secured,racecar_controlers.racecar_update_Page);

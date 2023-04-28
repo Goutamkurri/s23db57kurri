279bf09a33665d6a1e914b6fea055313fd3d2e64
Goutamkurri <123128235+Goutamkurri@users.noreply.github.com>
2023-04-27 21:42:24 -0500

    added security to create and delete

--- routes/racecar.js
+++ routes/racecar.js
@@ -17,11 +17,11 @@ module.exports = router;
 /* GET detail racecar page */
 router.get('/detail', racecar_controlers.racecar_view_one_Page);
 /* GET create racecar page */
-router.get('/create', racecar_controlers.racecar_create_Page);
+router.get('/create',secured, racecar_controlers.racecar_create_Page);
 /* GET create update page */
 // router.get('/update', racecar_controlers.racecar_update_Page);
 /* GET delete racecar page */
-router.get('/delete', racecar_controlers.racecar_delete_Page);
+router.get('/delete',secured, racecar_controlers.racecar_delete_Page);
 
 /* GET update racecar page */
 router.get('/update', secured,racecar_controlers.racecar_update_Page);
